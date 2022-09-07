aa864abae012486436c13837f86bb21c73ae17e1
Veer1498 <singavarapuveerendra@gmail.com>
2022-09-07 19:04:08 +0530

    [Veer] Added : Welcome Message

---
+++ Mood-Analyzer-Problem/src/main/java/moodanalyzer/MoodAnalyzer.java
@@ -0,0 +1,27 @@
+package moodanalyzer;
+
+/**
+ * To Check the Mood of the Person by String Entered.
+ * Emphasis on - Junit Test Cases - Exception Handling - Custom Exceptions
+ *
+ * Author @Veer
+ */
+//Mood Analyzer Class
+public class MoodAnalyzer {
+    //Welcome Function
+    public static void welcome(){
+        System.out.println("Welcome to Mood Analyzer");
+        System.out.println("========================");
+    }
+//    //Function to Check Mood of a String Entered
+//    public String moodCheck(String message){
+//        if(message.contains("SAD")) {
+//            System.out.println("In SAD Mood");
+//            return "SAD";
+//        }
+//        else{
+//            System.out.println("In HAPPY Mood");
+//            return "HAPPY";
+//        }
+//    }
+}
---
+++ Mood-Analyzer-Problem/src/test/java/moodanalyzer/MoodAnalyzerTest.java
@@ -0,0 +1,27 @@
+package moodanalyzer;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * To Check the Mood of the Person by String Entered.
+ * Emphasis on - Junit Test Cases - Exception Handling - Custom Exceptions
+ * Author @Veer
+ *
+ * Testing Class of MoodAnalyzerTest
+ * Created Object of Mood Analyzer
+ * Created a Test Function and Imported Assertions and checking SAD or HAPPY
+ */
+public class MoodAnalyzerTest {
+    MoodAnalyzer moodAnalyze = new MoodAnalyzer();
+
+    @Test
+    public void if_MessageWasSad_ShouldReturnSad(){
+        moodAnalyze.welcome();
+
+//        String message = "I am in SAD Mood";
+//       String mood = moodAnalyze.moodCheck(message);
+//        Assertions.assertEquals("SAD",mood);
+
+    }
+}
